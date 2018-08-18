db8063f9b7b3e6896bb40da85a3ab04ca828cccc
ReedS <ReedStock1992@gmail.com>
2018-08-17 18:30:09 -0700

    Recent Changes: - Fixed home button changing colors - Navbar items alighned to the right - Changed navbar height - Changed image height to fit screen
    To Do:
    - Banner
      - Add logo
      - Add phrase
    - Finish navbar
      - Add new home button - change
    - Finish About Me
      - Add small summary
    - Finish Project Examples
      - Add image
      - Small description of projects
    - Finish Contact Me
      - Add form for quick email
      - Add bar with links to social media
        - Linkedin
        - Facebook
        - Gmail
        - Etc.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,11 @@
+/* ==========================================================================
+   Reed's custom styles
+   ========================================================================== */
 
+function hover(element) {
+  element.setAttribute("src", "img/Light Bulb Blue.png");
+}
+
+function unhover(element) {
+  element.setAttribute("src", "img/Light Bulb.png");
+}
