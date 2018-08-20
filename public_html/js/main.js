621410e8942902d52a1ba5e781b3dccdf1ea76da
ReedS <ReedStock1992@gmail.com>
2018-08-20 01:46:25 -0700

    Recent Changes: - Fixed navbar home button rocket animation - Resized navbar home button - Replaced About Me page photo with better quality image - Continued work on grid spacing in About Me page - Began work on carousel for projects
    To Do:
    - Banner
      - Done
    - Finish navbar
      - Done
    - Finish About Me
      - Add small summary
    - Finish Project Examples
      - Add image
      - Small description of projects
    - Finish Contact Me
      - Add form for quick email
      - Add bar with links to social media
        - Linkedin
        - Facebook
        - Gmail
        - Etc.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -2,10 +2,14 @@
    Reed's custom styles
    ========================================================================== */
 
-function hover(element) {
-  element.setAttribute("src", "img/Rocket-Launching.png");
+// Functions to change home button color/image for rocket
+function hover(homeButton, rocketColor) {
+  rocketColor.setAttribute("src", "img/Rocket-Launching.png");
+  homeButton.style.color = "#66FCF1";
+}
+function unhover(homeButton, rocketColor) {
+  rocketColor.setAttribute("src", "img/Rocket.png");
+  homeButton.style.color = "#C5C6C7";
 }
 
-function unhover(element) {
-  element.setAttribute("src", "img/Rocket.png");
-}
+// Functions to change light bulb color.
