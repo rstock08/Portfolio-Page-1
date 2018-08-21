7ea75a7af86301762889f173b8f3566c271d3451
ReedS <ReedStock1992@gmail.com>
2018-08-20 18:11:19 -0700

    Recent Changes: - Added modal with PDF link
    To Do:
    - Banner
      - Done
    - Finish navbar
      - Done
    - Finish About Me
      - Add small summary
    - Finish Project Examples
      - Add image
      - Small description of projects
    - Finish Contact Me
      - Add bar with links to social media
        - Linkedin
        - Facebook
        - Gmail
        - Etc.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -12,4 +12,16 @@ function unhover(homeButton, rocketColor) {
   homeButton.style.color = "#C5C6C7";
 }
 
+// Function to change Resume modal button colors
+function hoverModal(modalColors) {
+    modalColors.style.color = "#1F2833";
+    modalColors.style.background = "#66FCF1";
+    modalColors.style.border = "#45A29E";
+}
+function unhoverModal (modalColors) {
+    modalColors.style.color = "#C5C6C7";
+    modalColors.style.background = "#45A29E";
+    modalColors.style.border = "#45A29E";
+}
+
 // Functions to change light bulb color.
