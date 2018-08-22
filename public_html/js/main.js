ffa81d62729f0fd1a8a1385949b6f9a18003d6e6
ReedS <ReedStock1992@gmail.com>
2018-08-22 00:11:12 -0700

    Recent Changes: - Added project example image overlays - Fixed small media formatting issues
    To Do:
    - Banner
      - Done
    - Finish navbar
      - Done
    - Finish About Me
      - Add small summary
    - Finish Project Examples
      - Add image
      - Small description of projects
    - Finish Contact Me
      - Add bar with links to social media
        - Linkedin
        - Facebook
        - Gmail
        - Etc.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -2,13 +2,23 @@
    Reed's custom styles
    ========================================================================== */
 
+/*
+Color Scheme:
+
+Black: #0B0C10 
+Navy Blue: #1F2833 
+Grey: #C5C6C7 
+Teal: #66FCF1 
+Dark Teal: #45A29E
+*/
+
 // Functions to change home button color/image for rocket
 function hover(homeButton, rocketColor) {
-  rocketColor.setAttribute("src", "img/Rocket-Launching.png");
+  rocketColor.setAttribute("src", "img/animation/Rocket-Launching.png");
   homeButton.style.color = "#66FCF1";
 }
 function unhover(homeButton, rocketColor) {
-  rocketColor.setAttribute("src", "img/Rocket.png");
+  rocketColor.setAttribute("src", "img/animation/Rocket.png");
   homeButton.style.color = "#C5C6C7";
 }
 
@@ -16,12 +26,8 @@ function unhover(homeButton, rocketColor) {
 function hoverModal(modalColors) {
     modalColors.style.color = "#1F2833";
     modalColors.style.background = "#66FCF1";
-    modalColors.style.border = "#45A29E";
 }
 function unhoverModal (modalColors) {
     modalColors.style.color = "#C5C6C7";
     modalColors.style.background = "#45A29E";
-    modalColors.style.border = "#45A29E";
 }
-
-// Functions to change light bulb color.
